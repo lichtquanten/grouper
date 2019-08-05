9bc72cc604a6bc16f4dba2210ba75ade1363da85
sean-hackett <seanhackett55@gmail.com>
2019-08-05 17:01:51 -0400

    Reinvent Block

--- src/grouper/grouper.py
+++ src/grouper/grouper.py
@@ -1,11 +1,8 @@
 """A module for grouping timestamped data."""
 
 from abc import ABCMeta, abstractmethod
-import collections
-import buffer
-import numpy as np
 
-class Grouper:
+class Grouper(object):
     __metaclass__ = ABCMeta
 
     def __init__(self):
@@ -15,8 +12,45 @@ class Grouper:
         return self
 
     @abstractmethod
+    def next(self):
+        pass
+
+    @abstractmethod
     def put(self, data, start_time, end_time):
         pass
+
+class Block(Grouper):
+    """Divides data into blocks of fixed length.
+
+    Wrapper to BlockBuffer that tracks start, end time of blocks.
+    Accepts timestamped input data. Produces timestamped blocks.
+
+    """
+    def __init__(self, block_size):
+        self._block_size = self._block_size
+        self._output_buffer = []
+        self._reset_data()
+
+    def _reset_data():
+        self._data = []
+        self._start_time = None
+        self._end_time = None
+
+    def next(self):
+        if not self._output_buffer:
+            raise StopIteration
+        return self._output_buffer.pop(0)
+
+    def put(self, datum, start_time, end_time):
+        self._data.append(datum)
+        if self._start_time is None:
+            self._start_time = start_time
+        self._end_time = end_time
+
+        if len(self._data) == self._block_size:
+            self._output_buffer.append(
+                (self._data, self._start_time, self._end_time))
+            self._reset_data()
 
 class Counter(Grouper):
     """Counts how many consecutive data satisfy `is_valid`.
@@ -72,112 +106,6 @@ class Counter(Grouper):
             self._counter += 1
         else:
             self._counter = 0
-
-class Window(Grouper):
-    """Divides timestamped data into windows of time of fixed duration.
-
-    All windows are offset from the initial start time by a multiple of the
-    window duration. The first window yielded is the earliest window into which
-    the first received datum falls.
-
-    Assumes that data are received in temporal order.
-    received in temporal order.
-    """
-    def __init__(self, start_time, window_duration):
-        """
-        Parameters
-        ----------
-        start_time : implements __add__, __sub__
-            The time from which the start time of all windows will be
-            offset by a multiple of `window_duration`.
-        window_duration : implements __add__, __sub__, __div__
-            The duration of each window.
-        """
-        self._start_time = start_time
-        self._window_duration = window_duration
-        self._windows = []
-        self._current = None
-
-    def next(self):
-        """
-        Returns
-        -------
-        list
-            A list of data in a window. A datum is considered in a window if
-            it overlaps in time with the window.
-        any
-            The start time of the window.
-        any
-            The end time of the window.
-
-        Raises
-        ------
-        StopIteration
-            When there are no more available windows.
-        """
-        if not self._windows:
-            raise StopIteration
-        window = self._windows.pop(0)
-        return (window['data'], window['start_time'], window['end_time'])
-
-    def _initialize_current(self):
-        """Find appropriate start time for first window, create first window."""
-        # Accelerate the start time, if needed
-        if start_time > self._start_time:
-            diff = start_time - self._start_time
-            self._start_time += self._window_duration * int(diff / self._window_duration)
-
-        # Initialize current
-        self._current = {
-            'start_time': self._start_time,
-            'end_time': self._start_time + self._window_duration,
-            'data': []
-        }
-
-    def _send_current(self):
-        """Adds the current window to the windows buffer. Creates a new current window.
-        """
-        new_window = {
-            'start_time': self._current['end_time'],
-            'end_time': self._current['end_time'] + self._window_duration,
-            'data': []
-        }
-        self._windows.append(self._current)
-        self._current = new_window
-
-    def put(self, datum, start_time, end_time):
-        """Evalute the validity of `datum` and update the counter.
-
-        `datum` assumed to be after, by timestamp, all previously put data.
-
-        Parameters
-        ----------
-        datum: any
-            Anything.
-        start_time : any
-            The start time associated with `datum`.
-        end_time : any
-            The end time associated with `datum`.
-        """
-        # Ignore data ending before current window starts
-        if end_time < self._start_time:
-            return
-
-        # True on first pass
-        if not self._current:
-            self._initialize_current()
-
-        # Ship window if it ends before the `datum` ends
-        while start_time > self._current['end_time']:
-            self._send_current()
-
-        # Add `datum` to and ship all windows that end before `datum` ends
-        while end_time > self._current['end_time']:
-            self._current['data'].append(datum)
-            self._send_current()
-
-        # Add `datum` to the window that in which its end time falls
-        self._current['data'].append(datum)
 
 class History(Grouper):
     """Provides a list of the last `length` data input before each datum.
@@ -329,56 +257,108 @@ class Neighborhood(Grouper):
                 self._output_buffer.append(
                     (False, first['start_time'], first['end_time']))
 
-class Block(Grouper):
-    """Divides data into blocks of fixed length.
+class Window(Grouper):
+    """Divides timestamped data into windows of time of fixed duration.
 
-    Wrapper to BlockBuffer that tracks start, end time of blocks.
-    Accepts timestamped input data. Produces timestamped blocks.
+    All windows are offset from the initial start time by a multiple of the
+    window duration. The first window yielded is the earliest window into which
+    the first received datum falls.
+
+    Assumes that data are received in temporal order.
+    received in temporal order.
     """
-    def __init__(self, block_size=None, block_buffer=None):
-        if block_size is None and block_buffer is None:
-            raise Exception('Must specify block_size or block_buffer')
-        if block_buffer is not None:
-            self._block_buffer = block_buffer
-        elif block_size is not None:
-            self._block_buffer = buffer.BlockBuffer(block_size)
-        # Change times to contain dictionaries, not tuples
-        self._times = []
-        self._last = None
-
-    def __iter__(self):
-        for block in self._block_buffer:
-            if not self._last:
-                self._last = self._times[0][1]
-            l = len(block)
-            while l > 0:
-                # somewhat important >= vs > decision
-                # This is absolutely unreadable
-                if self._times[0][0] >= l:
-                    t = self._len2time(l, *self._times[0])
-                    end = self._times[0][1] + t
-                    self._times[0][0] -= l
-                    self._times[0][1] += t
-                    l = 0
-                else:
-                    l -= self._times[0][0]
-                    del self._times[0]
-
-            yield (block, self._last, end)
-            self._last = end
-
-    @staticmethod
-    def _len2time(segment_length, length, start, end):
+    def __init__(self, start_time, window_duration):
         """
-        Args:
-            segment_length:
-            length:
-            start:
-            end:
+        Parameters
+        ----------
+        start_time : implements __add__, __sub__
+            The time from which the start time of all windows will be
+            offset by a multiple of `window_duration`.
+        window_duration : implements __add__, __sub__, __div__
+            The duration of each window.
         """
-        return (end - start) * float(segment_length) / length
+        self._start_time = start_time
+        self._window_duration = window_duration
+        self._windows = []
+        self._current = None
 
-    def put(self, data, start_time, end_time):
-        """Add data to the buffer."""
-        self._block_buffer.put(data)
-        self._times.append([len(data), start_time, end_time])
+    def next(self):
+        """
+        Returns
+        -------
+        list
+            A list of data in a window. A datum is considered in a window if
+            it overlaps in time with the window.
+        any
+            The start time of the window.
+        any
+            The end time of the window.
+
+        Raises
+        ------
+        StopIteration
+            When there are no more available windows.
+        """
+        if not self._windows:
+            raise StopIteration
+        window = self._windows.pop(0)
+        return (window['data'], window['start_time'], window['end_time'])
+
+    def _initialize_current(self):
+        """Find appropriate start time for first window, create first window."""
+        # Accelerate the start time, if needed
+        if start_time > self._start_time:
+            diff = start_time - self._start_time
+            self._start_time += self._window_duration * int(diff / self._window_duration)
+
+        # Initialize current
+        self._current = {
+            'start_time': self._start_time,
+            'end_time': self._start_time + self._window_duration,
+            'data': []
+        }
+
+    def _send_current(self):
+        """Adds the current window to the windows buffer. Creates a new current window.
+        """
+        new_window = {
+            'start_time': self._current['end_time'],
+            'end_time': self._current['end_time'] + self._window_duration,
+            'data': []
+        }
+        self._windows.append(self._current)
+        self._current = new_window
+
+    def put(self, datum, start_time, end_time):
+        """Evalute the validity of `datum` and update the counter.
+
+        `datum` assumed to be after, by timestamp, all previously put data.
+
+        Parameters
+        ----------
+        datum: any
+            Anything.
+        start_time : any
+            The start time associated with `datum`.
+        end_time : any
+            The end time associated with `datum`.
+        """
+        # Ignore data ending before current window starts
+        if end_time < self._start_time:
+            return
+
+        # True on first pass
+        if not self._current:
+            self._initialize_current()
+
+        # Ship window if it ends before the `datum` ends
+        while start_time > self._current['end_time']:
+            self._send_current()
+
+        # Add `datum` to and ship all windows that end before `datum` ends
+        while end_time > self._current['end_time']:
+            self._current['data'].append(datum)
+            self._send_current()
+
+        # Add `datum` to the window that in which its end time falls
+        self._current['data'].append(datum)
