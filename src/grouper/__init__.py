5c5d66a1f97d5f135a8f440ff7e757282011feee
sean-hackett <seanhackett55@gmail.com>
2019-09-24 15:02:10 -0400

    Rename Grouper_Cluster

--- src/grouper/__init__.py
+++ src/grouper/__init__.py
@@ -1,3 +1,3 @@
 from .aligner import Aligner
 from .combiner import Combiner
-from .grouper import Block, BlockArrLike, Counter, Grouper, Grouper_Cluster, History, Neighborhood, Window
+from .grouper import Block, BlockArrLike, Counter, Grouper, Group_Cluster, History, Neighborhood, Window
